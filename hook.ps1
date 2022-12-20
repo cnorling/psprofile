4665c561ff03838772b2af121c7f3cce5a7cb31a
salineselin <salineselin@gmail.com>
2022-12-20 03:49:42 -0700

    added oh my posh initialization to prompt

--- hook.ps1
+++ hook.ps1
@@ -1,3 +1,4 @@
 # this file acts as a hook to import my profile using the default location
 Import-Module "$env:USERPROFILE\git\psprofile\psprofile.psm1"
+oh-my-posh init pwsh | Invoke-Expression
 bgdc
